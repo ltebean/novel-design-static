d77a1fd7886549db4229887d37a14e54c44de053
Cong Yu <yucong1118@gmail.com>
2014-07-14 21:34:31 +0800

    in progress

--- dev/js/entries/index.js
+++ dev/js/entries/index.js
@@ -4,38 +4,87 @@ var fastclick = require('fastclick');
 var Spinner = require('spin.js');
 var IScroll = require('iscroll');
 
-fastclick(document.body);
-//new IScroll(document.body);
 
 
 var spinner = new Spinner({
   color: '#111111',
   lines: 10,
-  length: 10
+  length: 9,
+  width: 3
 });
 
 
-spinner.spin(document.body);
 
-$('.spinner').css({
-  position:'fixed'
-});
+function showSpinner() {
+  spinner.spin(document.body);
+  $('.spinner').css({
+    position: 'fixed'
+  });
+}
 
-setTimeout(function() {
+function hideSpinner() {
   spinner.stop()
-}, 2000);
+}
+
 
 seaport.connect(function dataHandler(data) {
-
   console.log('receive data:' + data);
-
-}, function(bridge) {
-
-	$('.product').on('click',function(){
-		bridge.data.send({})
-	})
+}, init);
 
 
+function init(bridge) {
+  var loading = false;
+  var wrapper = $('.design-list');
+  var template = $('.design-template');
+  var more = $('.more');
 
+  more.on('click', function() {
+    loadData();
+  })
 
-})
+  loadData();
+
+  var domain = 'noveldesign.apiary-mock.com';
+
+  function loadData(page) {
+    if (loading) {
+      return;
+    }
+    showSpinner();
+    loading = true
+    bridge.http.get({
+      domain: 'noveldesign.apiary-mock.com',
+      path: '/api/design'
+    }, function(data) {
+      hideSpinner();
+      loading = false;
+      if (!data) {
+        alert('network error');
+        return;
+      }
+      data.forEach(function(data) {
+        generateDom(data)
+      });
+      more.removeClass('hide');
+    })
+  }
+
+  function generateDom(data) {
+    var design = $(template.html());
+    design.find('.title').text(data.title);
+    design.find('.description').text(data.description);
+    design.on('click', function() {
+      bridge.data.send({})
+    });
+    design.appendTo(wrapper);
+    fastclick(design[0])
+    design.find('.thumb img')[0].onload = function() {
+      this.style.opacity = 1;
+      setTimeout(function() {
+        design.find('.likes').css('opacity', 1);
+      }, 1300);
+    }
+    design.find('.thumb img').attr('src', data.pics[0]);
+
+  }
+}
